7770f82da2ebed8e9c4a6972b2e5176aea038051
aidenbillingslea <kingaidenbill@gmail.com>
2026-02-05 10:18:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("enter your number")
-print("-----------------")
+print("Enter the amount of stars you want")
+print("----------------------------------")
 number = int(input("Enter a number: "))
-for i in range(number):
-    print("*")
+for i in range(1, number + 1):
+    print("                                             ௹" * i)
 print("Pattern Completed!")
